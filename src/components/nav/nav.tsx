338bb95623eec76a1af95851f1e38cd66781f392
Yeongi Park <yeongi0111@naver.com>
2024-09-24 17:01:07 +0900

    FAET : 내비게이션에 로고 추가

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -2,12 +2,23 @@
 import { useNavScroll } from "@/hooks/useNavScroll";
 import style from "./nav.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
+  const pathname = usePathname();
+
   const { isScrolled } = useNavScroll();
   return (
     <div className={style.wrap}>
       <div className={`${style.container} ${isScrolled ? style.scrolled : ""}`}>
+        <p className={style.logo}>
+          <Link href={"/"}>
+            SO
+            <br />
+            OP
+          </Link>
+        </p>
+
         <div className={style.buttons}>
           <p>
             <Link href={"/product"}>예약하기</Link>
