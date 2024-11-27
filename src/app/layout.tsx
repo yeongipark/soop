2d85d26038f5725bcaebb3c2fde6c04395f6fe3a
Yeongi Park <yeongi0111@naver.com>
2024-11-27 16:30:20 +0900

    style(home): 홈화면 배경 색, 글자색 변경, 홈 화면 글귀 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import "./globals.css";
 import RecoilWrapper from "@/components/recoilWrapper";
 
@@ -8,9 +11,20 @@ export default function RootLayout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }>) {
+  const pathname = usePathname(); // 현재 경로를 가져옴
+
+  const backgroundColor = pathname === "/" ? "var(--background)" : "#ffffff";
+
   return (
     <html lang="en">
-      <body>
+      <body
+        style={{
+          backgroundColor,
+          maxWidth: "600px",
+          margin: "auto",
+          minHeight: "100vh",
+        }}
+      >
         <RecoilWrapper>{children}</RecoilWrapper>
         {modal}
       </body>
