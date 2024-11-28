9e929fa8c3ff9b251b4ff778df4f03570acb9a29
Yeongi Park <yeongi0111@naver.com>
2024-11-28 14:02:59 +0900

    feat(notice): 공지사항 UI 완료

--- src/app/(footer)/product/page.tsx
+++ src/app/(footer)/product/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <div>
       <div className={style.text}>
         <p>
-          {">"}예약 전 반드시 <a>공지</a> 확인 부탁드립니다!{"<"}
+          {">"}예약 전 반드시 <a href="/notice">공지</a> 확인 부탁드립니다!{"<"}
         </p>
       </div>
       <ProductSearch />
--- src/components/notice/post.tsx
+++ src/components/notice/post.tsx
@@ -3,7 +3,9 @@ import style from "./post.module.css";
 export default function Post() {
   return (
     <div className={style.container}>
-      <p className={style.title}>📢 예약 전 필독!</p>
+      <p className={style.title}>
+        <a href={`/notice/detail`}>📢 예약 전 필독!</a>
+      </p>
       <p className={style.date}>2024.10.25</p>
     </div>
   );
