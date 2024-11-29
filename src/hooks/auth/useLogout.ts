4390ae06ca7ea1dceb9c4ce007f5566be50fd97a
Yeongi Park <yeongi0111@naver.com>
2024-11-30 00:28:58 +0900

    chore: 로그아웃 로직 axios로 변경

--- src/hooks/auth/useLogout.ts
+++ src/hooks/auth/useLogout.ts
@@ -1,7 +1,8 @@
 import { useRecoilState } from "recoil";
 import { isLoginState } from "@/recoil/isLoginAtom";
-import { deleteToken, getToken } from "@/util/cookie";
+import { deleteToken } from "@/util/cookie";
 import { useRouter } from "next/navigation";
+import apiClient from "@/util/axios";
 
 export default function useLogout() {
   const [_, setIsLogin] = useRecoilState(isLoginState);
@@ -10,27 +11,26 @@ export default function useLogout() {
   // 비동기 로그아웃 처리 함수
   const handleLogout = async () => {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/logout`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${getToken()}`,
-        },
-        credentials: "include",
+      const res = await apiClient("/api/logout", {
+        method: "delete",
+        withCredentials: true,
       });
 
-      if (!res.ok) {
-        throw new Error("로그아웃에 실패했습니다.");
-      }
+      // if (res.status !== 200) {
+      //   throw new Error("로그아웃에 실패했습니다.");
+      // }
 
       // 로그아웃 성공 처리
       setIsLogin(false);
-      // 캐시에서 삭제하기
+      // 캐시에서 토큰 삭제
       deleteToken();
+      // 로컬스토리지에서 토큰 삭제
+      localStorage.removeItem("accessToken");
 
-      // 로그아웃 하고 홈으로 리다이렉트
+      // 로그아웃 후 홈으로 리다이렉트
       router.replace("/");
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error("Unexpected Error during logout:", error);
     }
   };
 
