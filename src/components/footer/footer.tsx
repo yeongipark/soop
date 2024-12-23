6458a7d1303b5fd364a6d8cfee0238041aabbbbc
Yeongi Park <yeongi0111@naver.com>
2024-12-23 14:12:21 +0900

    feat: footer 사이트 연결결

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,16 +4,36 @@ export default function Footer() {
   return (
     <footer className={style.container}>
       <div>
-        <p>SOOP</p>
+        <p>re-bin</p>
         <p>
-          <a href="mailto:subin@naver.com">subin@naver.com</a> | 010.3381.2729
+          <a
+            href="mailto:re.bin.subin@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            re.bin.subin@gmail.com
+          </a>
         </p>
         <p>09:00 - 20:00 (Lunch Time 12:00 - 13:00)</p>
         <p>Hosting bt O & P</p>
       </div>
       <div className={style.sns}>
         <p>
-          <a>Instargram </a>| <a>kakao</a>
+          <a
+            href="https://www.instagram.com/re.bin___?igsh=cm85ZmR4ZmI2eGJt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instargram
+          </a>
+          |
+          <a
+            href="https://open.kakao.com/o/sxFtI9Yg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            kakao
+          </a>
         </p>
       </div>
     </footer>
