d870e8118ce10d351cabe9326b26d84b8a0a902c
Yeongi Park <yeongi0111@naver.com>
2024-12-25 10:26:37 +0900

    feat: 예약 확인 문의하기 버튼 연결결

--- src/components/reservationCheck/menuModal.tsx
+++ src/components/reservationCheck/menuModal.tsx
@@ -23,7 +23,14 @@ export default function MenuModal({
 
   return (
     <div className={style.container}>
-      <button>문의하기</button>
+      <button
+        onClick={() => {
+          window.open("https://open.kakao.com/o/sxFtI9Yg", "_blank");
+          setMenuModal(false);
+        }}
+      >
+        문의하기
+      </button>
       <button>예약번호 복사</button>
       <button onClick={handleCancel}>예약 취소하기</button>
     </div>
